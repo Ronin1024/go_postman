900ceaaaaae454b845d6a8962828a760d562343c
Ronin1024 <Ronin1024@bk.ru>
2026-05-04 00:20:58 +0300

    Changed

--- go_postman.go
+++ go_postman.go
@@ -14,11 +14,11 @@ import (
 type FetchOptions struct {
 	Method  string
 	Headers map[string]string
+	Timeout int
 }
 
-func Fetch(url string, postBody map[string]interface{}, options FetchOptions) (response *http.Response, err error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	defer cancel()
+func Fetch(url string, postBody map[string]interface{}, options FetchOptions) (response *http.Response, cancel context.CancelFunc, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(options.Timeout)*time.Second)
 
 	requestBodyBytes, _ := json.Marshal(postBody)
 	requestBody := bytes.NewBuffer(requestBodyBytes)
@@ -27,6 +27,7 @@ func Fetch(url string, postBody map[string]interface{}, options FetchOptions) (r
 		req, err = http.NewRequestWithContext(ctx, strings.ToUpper(options.Method), url, nil)
 	}
 	if err != nil {
+		cancel() // отменяем контекст только при ошибке
 		err = fmt.Errorf("could not create request: %s\n", err)
 		return
 	}
@@ -36,6 +37,7 @@ func Fetch(url string, postBody map[string]interface{}, options FetchOptions) (r
 
 	response, err = http.DefaultClient.Do(req)
 	if err != nil {
+		cancel() // отменяем контекст только при ошибке
 		if errors.Is(ctx.Err(), context.DeadlineExceeded) {
 			err = fmt.Errorf("Timeout abort: %s\n", err)
 			return
